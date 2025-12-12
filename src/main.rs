281f02d7a4d2b162de9a7d9d861a5cb989dd658b
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2025-12-12 16:04:51 +0800

    Implement exit #pn5

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
+        if command.trim() == "exit" {
+            break;
+        }
         println!("{}: command not found", command.trim());
+
     }
 }
