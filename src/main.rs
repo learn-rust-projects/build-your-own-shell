2fe11d72d9f6e4a27aec59374aef912ae8ed0cb4
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2025-12-12 16:25:35 +0800

    Implement type #ez5

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    // TODO: unwrap 使用context优化
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -13,6 +14,13 @@ fn main() {
             break;
         }else if command_trim.starts_with("echo") {
             println!("{}", command_trim.strip_prefix("echo ").unwrap());
+        }else if command_trim.starts_with("type") {
+            let command_type = command_trim.strip_prefix("type ").unwrap();
+            if command_type == "exit" || command_type == "echo" || command_type == "type" {
+                println!("{} is a shell builtin", command_type);
+            }else{
+                println!("{command_type}: not found");
+            }
         }else{
             println!("{}: command not found", command_trim);
         }
