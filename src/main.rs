6dabbc8686c4c30391ad094d30ff6ba5598cd739
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2025-12-21 19:08:01 +0800

    Implement Quoting 1. Single quotes 2. Double quotes 3. Backslash outside quotes 4. Backslash within single quotes 5. Backslash within double quotes 6. Executing a quoted executable

--- src/main.rs
+++ src/main.rs
@@ -28,25 +28,20 @@ fn main() -> anyhow::Result<()> {
 
         let line_trim = line.trim();
 
-        let mut params = line_trim.split_whitespace();
-        let command = params.next().context("command is empty")?.trim();
+        let mut params = split_quotes(line_trim);
+        let command = params.next().context("command is empty")?;
 
-        match command {
+        match command.as_str() {
             "exit" => break 'outer,
-            "echo" => println!(
-                "{}",
-                line_trim
-                    .strip_prefix("echo ")
-                    .context("echo command is empty")?
-            ),
+            "echo" => println!("{}", params.collect::<Vec<_>>().join(" ")),
             "type" => {
-                let command_type = params.next().context("type command is empty")?.trim();
+                let command_type = params.next().context("type command is empty")?;
                 // TODO:使用 enum 优化
-                match command_type {
+                match command_type.as_str() {
                     "exit" | "echo" | "type" | "pwd" | "cd" => {
                         println!("{} is a shell builtin", command_type)
                     }
-                    _ => match find_executable_file_in_paths(command_type, &GLOBAL_VEC) {
+                    _ => match find_executable_file_in_paths(&command_type, &GLOBAL_VEC) {
                         Some(file_path) => println!("{} is {}", command_type, file_path.display()),
                         None => println!("{command_type}: not found"),
                     },
@@ -58,18 +53,18 @@ fn main() -> anyhow::Result<()> {
             ),
 
             "cd" => {
-                let dir = params.next().context("cd command is empty")?.trim();
+                let dir = params.next().context("cd command is empty")?;
                 if params.next().is_some() {
                     println!("bash: cd: too many arguments");
                     break 'outer;
                 }
-                let dir = if dir == "~" { &HOME_DIR } else { dir };
+                let dir = if dir == "~" { &HOME_DIR } else { &dir };
                 match std::env::set_current_dir(dir).context("cd failed") {
                     Ok(_) => {}
                     Err(_) => println!("cd: {}: No such file or directory", dir),
                 }
             }
-            _ => match find_executable_file_in_paths(command, &GLOBAL_VEC) {
+            _ => match find_executable_file_in_paths(&command, &GLOBAL_VEC) {
                 Some(file_path) => {
                     let _ = Command::new(file_path.file_name().context("file name is empty")?)
                         .args(params)
@@ -100,4 +95,82 @@ fn find_executable_file_in_paths(executable_file: &str, paths: &Vec<PathBuf>) ->
         }
     }
     None
+}
+enum MatchType {
+    Default,
+    DoubleQuote,
+    SingleQuote,
+    Escaping,
+    DoubleQuoteEscaping,
+}
+
+fn split_quotes(line: &str) -> impl Iterator<Item = String> {
+    let mut params = Vec::new();
+    let mut string = String::new();
+    let mut match_type = MatchType::Default;
+    for ch in line.chars() {
+        match match_type {
+            MatchType::Default => match ch {
+                ch if ch.is_whitespace() => {
+                    if !string.is_empty() {
+                        params.push(string.clone());
+                        string = String::new();
+                    }
+                    continue;
+                }
+                '\'' => match_type = MatchType::SingleQuote,
+                '"' => match_type = MatchType::DoubleQuote,
+                '\\' => match_type = MatchType::Escaping,
+                _ => string.push(ch),
+            },
+            MatchType::SingleQuote => match ch {
+                '\'' => match_type = MatchType::Default,
+                _ => string.push(ch),
+            },
+            MatchType::DoubleQuote => match ch {
+                '"' => match_type = MatchType::Default,
+                '\\' => match_type = MatchType::DoubleQuoteEscaping,
+                _ => string.push(ch),
+            },
+            MatchType::DoubleQuoteEscaping => match ch {
+                '"' => {
+                    string.push(ch);
+                    match_type = MatchType::DoubleQuote;
+                }
+                '\\' => {
+                    string.push(ch);
+                    match_type = MatchType::DoubleQuote;
+                }
+                _ => {
+                    string.push('\\');
+                    string.push(ch);
+                    match_type = MatchType::DoubleQuote;
+                }
+            },
+            MatchType::Escaping => match ch {
+                _ => {
+                    string.push(ch);
+                    match_type = MatchType::Default;
+                }
+            },
+        }
+    }
+    if !string.is_empty() {
+        params.push(string.clone());
+    }
+    params.into_iter()
+}
+
+#[test]
+fn test_split_quotes() {
+    let line = "echo 'test     script' 'hello''example' shell''world";
+    let params = split_quotes(line).collect::<Vec<_>>();
+    assert_eq!(
+        params,
+        vec!["echo", "test     script", "helloexample", "shellworld"]
+    );
+
+    let line = "echo world     test";
+    let params = split_quotes(line).collect::<Vec<_>>();
+    assert_eq!(params, vec!["echo", "world", "test"]);
 }
